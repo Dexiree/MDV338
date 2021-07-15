115ee73690df6118ccb17df5a4f5794b2521b076
Dexiree <dexicup@gmail.com>
2021-07-14 20:51:45 -0400

    Begin Project
    Created the project

---
+++ DexireeColon_ScheduleApp/DexireeColon_ScheduleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DexireeColon_ScheduleApp
+//
+//  Created by Dexiree Colon on 7/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
