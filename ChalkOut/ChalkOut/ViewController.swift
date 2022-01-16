269f7eee8a04d42f2f1afddcda5a3b71ea5382eb
Dexiree <dexicup@gmail.com>
2022-01-16 12:17:45 -0500

    Adding PencilKit
    I got the canvas to draw, but the tool picker is not showing up.

--- ChalkOut/ChalkOut/ViewController.swift
+++ ChalkOut/ChalkOut/ViewController.swift
@@ -6,12 +6,37 @@
 //
 
 import UIKit
+import PencilKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
+    
+    // OUTLETS
+    @IBOutlet weak var canvasView: PKCanvasView!
+    
+    // Variables
+    var drawing = PKDrawing()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // set canvas delegate to self
+        canvasView.delegate = self
+        canvasView.drawing = drawing
+        
+        // for testing purposes allow fingers to draw
+        canvasView.drawingPolicy = .anyInput
+        
+        // tool picker
+        let toolPicker = PKToolPicker()
+        toolPicker.setVisible(true, forFirstResponder: canvasView)
+        toolPicker.addObserver(canvasView)
+//        if let window = parent?.view.window {
+//            let toolPicker = PKToolPicker()
+//            toolPicker.setVisible(true, forFirstResponder: canvasView)
+//            toolPicker.addObserver(canvasView)
+//
+//            canvasView.becomeFirstResponder()
+//        }
     }
 
 
