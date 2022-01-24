e4324c3de0b6b1990cf9c430ac9b534a092c7dd7
Dexiree <dexicup@gmail.com>
2022-01-24 11:25:39 -0500

    Added Login / Sign Up
    Added the ability to login and sign up using Firebase Auth

--- ChalkOut/ProjectsViewController.swift
+++ ChalkOut/ProjectsViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseStorage
+import FirebaseAuth
 import PencilKit
 
 class ProjectsViewController: UIViewController {
@@ -14,11 +15,21 @@ class ProjectsViewController: UIViewController {
     // OUTLETS
     @IBOutlet weak var projectsCollection: UICollectionView!
     
+    @IBOutlet weak var loginSignup: UILabel!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginCreateBtnName: UIButton!
+    @IBOutlet weak var loginSignupView: UIView!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
     // VARIABLES
     private let storage = Storage.storage().reference()
     var numOfProjects = 0
     var snapshots = [UIImage(systemName: "scribble")]
     var projects = [String]()
+    let animation = Animations()
+    var willLogin = false
     
 
     override func viewDidLoad() {
@@ -42,6 +53,10 @@ class ProjectsViewController: UIViewController {
         projectsCollection.delegate = self
         projectsCollection.dataSource = self
         
+        // popup
+        loginSignupView.bounds = CGRect(x: 0, y: 0, width: self.view.bounds.width * 0.5, height: self.view.bounds.height * 0.5)
+        loginSignupView.layer.cornerRadius = 20
+        
         // load data from database
         storage.child("Projects").listAll { list, error in
             guard error == nil else {
@@ -54,47 +69,33 @@ class ProjectsViewController: UIViewController {
             // getting the name of each project in the database
             for prefixes in list.prefixes {
                 self.projects.append(prefixes.name)
+                self.loadImage(file: prefixes.name)
             }
-            
-            self.loadImage()
         }
         
     }
     
-    func loadImage(){
+    func loadImage(file: String){
         snapshots.removeAll()
-        // TODO: Testing if image is downloading (delete later)
-        for project in projects {
-            storage.child("Projects/\(project)/snapshot.png").downloadURL { url, error in
-                guard let url = url, error == nil else {
-                    return
-                }
-
-                let urlString = url.absoluteString
-                print("Download: \(urlString)")
-            }
+        
+        // getting snapshot from database
+        storage.child("Projects/\(file)/snapshot.png").getData(maxSize: 10 * 1024 * 1024) { data, error in
             
-            // getting snapshot from database
-            storage.child("Projects/\(project)/snapshot.png").getData(maxSize: 10 * 1024 * 1024) { data, error in
+            // if error
+            guard let data = data, error == nil else {
+                print("There was an issue")
+                return
+            }
+
+            // get data
+            if let snapshot = UIImage(data: data){
+                self.snapshots.append(snapshot)
                 
-                // if error
-                guard let data = data, error == nil else {
-                    print("There was an issue")
-                    return
-                }
-
-                // get data
-                if let snapshot = UIImage(data: data){
-                    self.snapshots.append(snapshot)
-                    
-                    
-                }
-                // reload collectionView
-                if self.snapshots.count == self.projects.count{
-                    self.projectsCollection.reloadData()
-                }
             }
-            
+            // reload collectionView
+            if self.snapshots.count == self.projects.count{
+                self.projectsCollection.reloadData()
+            }
         }
         
     }
@@ -123,6 +124,8 @@ class ProjectsViewController: UIViewController {
             // save palette
             mainView.savePalette(data: newPalette)
             
+            //loadSketch(project: newName, for: mainView)
+            
         }
         
         // pop view
@@ -130,9 +133,89 @@ class ProjectsViewController: UIViewController {
         
     }
     
+    // MARK: - LOGIN / SIGN UP
+    @IBAction func showLoginSignup(_ sender: UIButton) {
+        
+        // open popup
+        animation.animateIn(desiredView: loginSignupView, on: self.view)
+        
+    }
+    
+    @IBAction func loginSignupSegments(_ sender: UISegmentedControl) {
+        let choice = sender.selectedSegmentIndex
+        
+        switch choice {
+        case 0:
+            willLogin = true
+            loginCreateBtnName.titleLabel?.text = "Login"
+            loginSignup.text = "Login"
+        case 1:
+            willLogin = false
+            loginCreateBtnName.titleLabel?.text = "Create Account"
+            loginSignup.text = "Sign Up"
+        default:
+            return
+        }
+
+    }
+    
+    @IBAction func loginSignupBtn(_ sender: UIButton) {
+        
+        switch willLogin {
+        case true:
+            login()
+        case false:
+            signup()
+        }
+        
+    }
+    
+    @IBAction func cancelBtn(_ sender: UIButton) {
+        animation.animateOut(desiredView: loginSignupView)
+    }
+    
+    func signup(){
+        if !(emailField.text!.isEmpty) || !(passwordField.text!.isEmpty) {
+            Auth.auth().createUser(withEmail: "\(String(describing: emailField.text))", password: "\(String(describing: passwordField.text))") { result, error in
+                
+                // check error
+                guard error == nil else {
+                    print("ERROR SIGNUP: \(String(describing: error))")
+                    self.errorLabel.text = "error signing up"
+                    self.errorLabel.textColor = .red
+                    return
+                }
+                
+                // success
+                print("Succeful Signed up as: \(String(describing: result?.user.uid))")
+                self.animation.animateOut(desiredView: self.loginSignupView)
+                
+            }
+        } else {
+            
+        }
+    }
+    func login(){
+        Auth.auth().signIn(withEmail: "myEmail@gmail.com", password: "password") { result, error in
+            
+            // check error
+            guard error == nil else {
+                print("ERROR LOGIN: \(String(describing: error))")
+                self.errorLabel.text = "error logging in"
+                self.errorLabel.textColor = .red
+                return
+            }
+            
+            // success
+            print("Successfully Logged in as: \(String(describing: result?.user.uid))")
+            self.animation.animateOut(desiredView: self.loginSignupView)
+        }
+    }
 
 }
 
+
+// MARK: - CollectionView
 // selected item
 extension ProjectsViewController: UICollectionViewDelegate {
     
@@ -152,63 +235,10 @@ extension ProjectsViewController: UICollectionViewDelegate {
             mainView.projectName = chosenProject
             
             // load data as drawing
-            storage.child("Projects/\(chosenProject)/sketch.drawing").getData(maxSize: 10 * 1024 * 1024) { data, error in
-
-                // if error
-                guard let data = data, error == nil else {
-                    print("There was an issue")
-                    return
-                }
-
-                // get sketch data
-                if let loadDrawing = try? PKDrawing(data: data){
-                    mainView.canvasView.drawing = loadDrawing
-                }
-            }
+            loadSketch(project: chosenProject, for: mainView)
             
-            // removes all colors from pallete if any
-            for color in mainView.pallete.subviews {
-                color.removeFromSuperview()
-            }
-            
-            // load palette data
-            storage.child("Projects/\(chosenProject)/palette.txt").getData(maxSize: 10 * 1024 * 1024) { data, error in
-
-                // if error
-                guard let data = data, error == nil else {
-                    print("There was an issue")
-                    return
-                }
-
-                // get color data
-                if let paletteString = String(data: data, encoding: .utf16) {
-                    
-                    if paletteString != "" {
-                        
-                        // each hex code has 7 char (EX. #FFFFFF)
-                        let numOfColors = paletteString.count / 7
-                        
-                        var start = 1
-                        var endBefore = 7
-                        var colors = [UIColor]()
-                        
-                        // seperating each color into an array
-                        for _ in 1...numOfColors {
-                            let hexColor = paletteString[start ..< endBefore]
-                            colors.append(UIColor().convertRGB(from: hexColor))
-                            
-                            start += 7
-                            endBefore += 7
-                        }
-                        
-                        // add colors to palette
-                        colors.forEach { color in
-                            mainView.new(color: color)
-                        }
-                    }
-                    
-                }
-            }
+            // load data as palette
+            loadPalette(project: chosenProject, for: mainView)
             
         }
         
@@ -216,7 +246,69 @@ extension ProjectsViewController: UICollectionViewDelegate {
         navigationController?.popViewController(animated: true)
         
     }
+    
+    func loadSketch(project: String, for view: ViewController) {
+        storage.child("Projects/\(project)/sketch.drawing").getData(maxSize: 10 * 1024 * 1024) { data, error in
+
+            // if error
+            guard let data = data, error == nil else {
+                print("There was an issue")
+                return
+            }
+
+            // get sketch data
+            if let loadDrawing = try? PKDrawing(data: data){
+                view.canvasView.drawing = loadDrawing
+            }
+        }
+    }
+    
+    func loadPalette(project: String, for view: ViewController) {
+        
+        // removes all colors from pallete if any
+        for color in view.pallete.subviews {
+            color.removeFromSuperview()
+        }
+        storage.child("Projects/\(project)/palette.txt").getData(maxSize: 10 * 1024 * 1024) { data, error in
+
+            // if error
+            guard let data = data, error == nil else {
+                print("There was an issue")
+                return
+            }
+
+            // get color data
+            if let paletteString = String(data: data, encoding: .utf16) {
+                
+                if paletteString != "" {
+                    
+                    // each hex code has 7 char (EX. #FFFFFF)
+                    let numOfColors = paletteString.count / 7
+                    
+                    var start = 1
+                    var endBefore = 7
+                    var colors = [UIColor]()
+                    
+                    // seperating each color into an array
+                    for _ in 1...numOfColors {
+                        let hexColor = paletteString[start ..< endBefore]
+                        colors.append(UIColor().convertRGB(from: hexColor))
+                        
+                        start += 7
+                        endBefore += 7
+                    }
+                    
+                    // add colors to palette
+                    colors.forEach { color in
+                        view.new(color: color)
+                    }
+                }
+                
+            }
+        }
+    }
 }
+
 // cell info
 extension ProjectsViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -227,7 +319,6 @@ extension ProjectsViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = projectsCollection.dequeueReusableCell(withReuseIdentifier: ProjectsCollectionViewCell.identifier, for: indexPath) as! ProjectsCollectionViewCell
         
-        //snapshots.append(thumbnail!)
         print("PROJECTS: \(projects.count) SNAPSHOTS: \(snapshots.count)")
         let i = (indexPath[0] * 4) + indexPath[1]
         print(i)
@@ -237,10 +328,4 @@ extension ProjectsViewController: UICollectionViewDataSource {
     }
     
     
-}
-// cell style
-extension ProjectsViewController: UICollectionViewDelegateFlowLayout {
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        return CGSize(width: 120, height: 120)
-//    }
 }
